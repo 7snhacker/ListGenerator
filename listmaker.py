aa260fb4d737971dd8031cee99312f43582ea379
7snhacker <74249353+7snhacker@users.noreply.github.com>
2021-04-22 11:16:46 +0300

    Add files via upload

---
+++ listmaker.py
@@ -0,0 +1,20 @@
+print("""  _     ___ ____ _____ ____ _____ _   _ 
+ | |   |_ _/ ___|_   _/ ___| ____| \ | |
+ | |    | |\___ \ | || |  _|  _| |  \| |
+ | |___ | | ___) || || |_| | |___| |\  |
+ |_____|___|____/ |_| \____|_____|_| \_|
+                                        """)
+print("instagram : 7snhacker")
+import string
+import random
+letters = string.ascii_lowercase
+r = open("list.txt", "w")
+li = 0
+while li == 0:
+    r.write( ''.join(random.choice(letters) for i in range(4))+'\r\n')
+    
+
+        
+r.close
+ 
+    
