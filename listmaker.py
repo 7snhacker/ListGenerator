c71101ac060e09ec512370e7aa67134065c39d78
7snhacker <74249353+7snhacker@users.noreply.github.com>
2021-06-24 06:08:12 +0300

    Update listmaker.py

--- listmaker.py
+++ listmaker.py
@@ -1,3 +1,4 @@
+
 print("""  _     ___ ____ _____ ____ _____ _   _ 
  | |   |_ _/ ___|_   _/ ___| ____| \ | |
  | |    | |\___ \ | || |  _|  _| |  \| |
@@ -8,13 +9,14 @@ print("instagram : 7snhacker")
 import string
 import random
 letters = string.ascii_lowercase
+user = int(input("letter count : "))
 r = open("list.txt", "w")
+done = 0
 li = 0
 while li == 0:
-    r.write( ''.join(random.choice(letters) for i in range(4))+'\r\n')
-    
+    done += 1
+    print(done)
+    r.write(''.join(random.choice(letters) for i in range(user)) + '\n')
 
-        
 r.close()
- 
-    
+
